d79a72df444585d1cc9edf9f3c36f37bfa8e9c9c
Meireles <junior_turren@hotmail.com>
2023-09-25 20:31:28 -0300

    components dinamicos proprertys

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,9 +1,10 @@
+import { AppComponent } from './../../app.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrls: ['./menu-title.component.css']
+  styleUrls: ['./menu-title.component.css', './menu-title.responsive.component.css']
 })
 export class MenuTitleComponent {
 
